6af49a0714062fae4527372f719d78940d13c77a
Patrik Farkas <farkas.patrik105@gmail.com>
2024-10-25 13:27:06 +0200

    Adds Dotenv as well and sorts packages

--- index.php
+++ index.php
@@ -5,5 +5,9 @@ declare(strict_types=1);
 require __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\VarDumper\VarDumper;
+use Symfony\Component\Dotenv\Dotenv;
 
-VarDumper::dump("Hello!");
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/.env');
+
+VarDumper::dump($_ENV);
